54603cb2bfc3b9f21eee337ea4e4a4d44ad458b6
Hirusha Fernando <heshanhfernando@gmail.com>
2024-03-19 18:08:03 +0530

    Model building

---
+++ text_pipeline.py
@@ -0,0 +1,57 @@
+import re
+import string
+
+import nltk
+import numpy as np
+from nltk.stem import WordNetLemmatizer
+from numpy import ndarray
+
+nltk.download('stopwords')
+nltk.download('wordnet')
+class TextPipeline:
+    def __init__(self, text):
+        self.__wnl = WordNetLemmatizer()
+        self.__stopwords_str = ", ".join(nltk.corpus.stopwords.words('english'))
+        self.text = text
+
+    def _preprocess(self):
+        self.text = self.text.lower()
+        self.text = re.sub("<.+?/?>", "", self.text)
+        self.text = self.text.translate(str.maketrans("", "", string.punctuation))
+        self.text = re.sub(r"\d+", "", self.text)
+        self.text = re.sub(r"  ", " ", self.text)
+
+    def _remove_stopwords(self):
+        new_text = []
+        for token in self.text.split():
+            if not token in self.__stopwords_str:
+                new_text.append(token)
+        self.text = " ".join(new_text)
+
+    def _lemmatize(self):
+        new_text = []
+        for word in self.text.split():
+            word = self.__wnl.lemmatize(word)
+            new_text.append(word)
+        self.text = " ".join(new_text)
+
+    def preprocess_text(self) -> str:
+        self._preprocess()
+        self._remove_stopwords()
+        self._lemmatize()
+        return self.text
+
+    def vectorize_text(self) -> ndarray:
+        with open("./artifacts/vocab.txt", "r") as file:
+            vocab = file.readlines()
+        vector_array = []
+        vector = np.zeros(len(vocab))
+        words = self.text.split()
+        for i in range(len(vocab)):
+            if vocab[i] in words:
+                vector[i] = 1
+        print(len(vector))
+        vector_array.append(vector)
+        np_array = np.array(vector_array, dtype=np.float32)
+        return np_array
+
